4bb020328e54c608f6715497d8d83970f69d99e7
pranaav88 <_gotawala@fanshaweonline.ca>
2023-10-09 18:36:41 -0400

    script change

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
 
-window.addEventListener('online', () => {
+if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-    .register("/service-worker.js", { scope: "/" })
-    .then((registration) => {
-      console.log("Register Success", registration);
-    })
-    .catch((error) => {
-      console.log("Register Failed", error);
-    });
-})
-
-window.addEventListener("load", () => {
-  if (!navigator.onLine) {
-    offlineMessage(); 
+      .register('/service-worker.js', { scope: '/' })
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
   }
-});
-
-function offlineMessage() {
-  const output = document.getElementById("main-cont");
-  output.innerHTML = `
-    <div id="offline">
-        <div id="offline-content">
-          <h3>No Connection</h3>
-          <p>Sorry!, but this page is not available offline.</p>
-        </div>
-    </div>
-    `;
-}
+  
+  window.addEventListener('load', () => {
+    if (!navigator.onLine) {
+      offlineMessage();
+    }
+  });
+  
+  function offlineMessage() {
+    const output = document.getElementById('main-cont');
+    output.innerHTML = `
+      <div id="offline">
+          <div id="offline-content">
+            <h3>No Connection</h3>
+            <p>Sorry, but this page is not available offline.</p>
+          </div>
+      </div>
+      `;
+  }
+  
