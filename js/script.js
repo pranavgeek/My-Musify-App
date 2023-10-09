45a04ceade00a4b04d379b8aa0dae836b2c8f214
pranaav88 <_gotawala@fanshaweonline.ca>
2023-10-09 17:27:39 -0400

    cache changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js', {scope: '/'})
-    .then((registration) => {
-        console.log("Register Success", registration)
-    })
-    .catch((error) => {
-        console.log("Register Failed", error)
-    })
-}
-else {
-    console.log("Service workers is not supported.")
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("/service-worker.js", { scope: "/" })
+//     .then((registration) => {
+//       console.log("Register Success", registration);
+//     })
+//     .catch((error) => {
+//       console.log("Register Failed", error);
+//     });
+// } else {
+//   console.log("Service workers is not supported.");
+// }
+
+window.addEventListener("load", () => {
+  if (navigator.onLine) {
+    navigator.serviceWorker
+      .register("/service-worker.js", { scope: "/" })
+      .then((registration) => {
+        console.log("Register Success", registration);
+      })
+      .catch((error) => {
+        console.log("Register Failed", error);
+      });
+  } else {
+    offlinepage();
+  }
+});
+
+function offlinepage() {
+  const output = document.getElementById("main-cont");
+  output.innerHTML = `
+    <div id="offline">
+        <div id="offline-content">
+          <h3>No Connection</h3>
+          <p>Sorry!, but this page is not available offline.</p>
+        </div>
+    </div>
+    `;
 }
--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,46 @@
-self.addEventListener('install', (event)=> {
-    console.log('[SW] Install!:', event)
+const cacheName = "cacheAssets-v10";
 
-    self.skipWaiting();
-})
+self.addEventListener("install", (event) => {
+  console.log("[SW] Install!:", event);
 
-self.addEventListener('activate', (event)=> {
-    console.log('[SW] Activate:', event)
+  self.skipWaiting();
 
-    event.waitUntil(clients.claim());
-})
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then(function (cache) {
+        cache.addAll([
+          "/",
+          "/index.html",
+          "/js/script.js",
+          "/main.css",
+          "/Icon/music-icon.png",
+          "/manifest.webmanifest",
+          "/magic.js"
+        ]);
+      })
+      .cache((error) => {
+        console.log("Cache Failed:", error);
+      })
+  );
+});
 
+self.addEventListener("activate", (event) => {
+  console.log("[SW] Activate:", event);
 
-self.addEventListener('fetch', ()=> {
-    return;
-})
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.open(cacheName).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        const fetchedRes = fetch(event.request).then((networkRes) => {
+          cache.put(event.request, networkRes.clone());
+          return networkRes;
+        });
+        return cachedResponse || fetchedRes;
+      });
+    })
+  );
+});
